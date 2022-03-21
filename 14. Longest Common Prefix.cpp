ea3dd8e495b330212d975d9866898a23c0372fe4
xuan <danielopg11@gmail.com>
2022-03-21 16:03:54 +0800

    all

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        string s;
+        s = strs[0];
+        for(int i = 0; i < strs.size(); i++){
+            string x;
+            for(int j = 0; j < strs[i].size(); j++){
+                if(s[j] == strs[i][j]){
+                    x += strs[i][j];
+                }  
+                else 
+                    break;
+            }
+            s = x;
+        }
+        return s;
+    }
+};
---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int number = s.size();
+        int i = 0;
+        for (; i < number; ++i) {
+        string x = "";
+        if (s[i] == '(' && s[i + 1] == ')') {
+            int num = i;
+            for (i += 2; i < number; i++) {
+                x += s[i];
+            }
+            s.replace(num,number, x);
+            i = -1;
+            number -= 2;
+        }
+        else if(s[i] == '[' && s[i+1] == ']'){
+            int num = i;
+            for(i += 2; i < number; i++){
+                x +=s [i];
+            }
+            s.replace(num, number, x);
+            i = -1;
+            number -= 2;;
+            }   
+        else if(s[i] == '{' && s[i+1] == '}'){
+            int num = i;
+            for(i += 2; i < number; i++){
+                x+=s[i];
+            }
+            s.replace(num, number, x);
+            i = -1;
+            number -= 2;
+        } 
+    }
+        if(s == "") return true;
+        else return false;
+    }
+};
